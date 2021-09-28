db5e4bfceb6d0520405391808fd33561d57d205b
Jyothi <jyothisurampudi54@gmail.com>
2021-09-28 21:39:19 +0530

    Initial commit

---
+++ events.js
@@ -0,0 +1,16 @@
+
+    function over()
+    {
+        alert("Mouse Over");
+    }
+    function sayHello()
+    {
+        alert("hello world");
+    }
+    function out(){
+        alert("mouse out");
+    }
+    function submit()
+    {
+        alert("submitted successfully");
+    }
